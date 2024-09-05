0ffbcd9bca2f16e78de3081760ffad98384298b1
roops92 <madridskate1399@gmail.com>
2024-09-05 15:06:16 +0900

    2024/09/05

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -107,6 +107,24 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
+
+        <a
+          href="http://localhost:3000/TTT"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            三目並べ{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
+            三目並べページに遷移します。
+          </p>
+        </a>
+
       </div>
     </main>
   );
