99bae4b612779ba04a7e4f3ddd9f504566ee9bb7
roops92 <madridskate1399@gmail.com>
2024-09-13 13:32:50 +0900

    レイアウト変更

--- my-app/app/TTT/page.tsx
+++ my-app/app/TTT/page.tsx
@@ -39,24 +39,65 @@ export default function Board() {
   }
 
   return (
-    <>
-      <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value = {squares[0]} onSquareClick={ () => handleClick(0)} />
-        <Square value = {squares[1]} onSquareClick={ () => handleClick(1)} />
-        <Square value = {squares[2]} onSquareClick={ () => handleClick(2)} />
+
+    <main className="flex min-h-screen flex-col items-center justify-start p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Get started by editing&nbsp;
+          <code className="font-mono font-bold">app/page.tsx</code>
+        </p>
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
+          <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{" "}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
       </div>
-      <div className="board-row">
-        <Square value = {squares[3]} onSquareClick={ () => handleClick(3)} />
-        <Square value = {squares[4]} onSquareClick={ () => handleClick(4)} />
-        <Square value = {squares[5]} onSquareClick={ () => handleClick(5)} />
+      
+      <div className="flex min-h-screen flex-col items-center justify-start p-24">
+        <div className="status">{status}</div>
+          <div className="board-row">
+            <Square value = {squares[0]} onSquareClick={ () => handleClick(0)} />
+            <Square value = {squares[1]} onSquareClick={ () => handleClick(1)} />
+            <Square value = {squares[2]} onSquareClick={ () => handleClick(2)} />
+          </div>
+        <div className="board-row">
+          <Square value = {squares[3]} onSquareClick={ () => handleClick(3)} />
+          <Square value = {squares[4]} onSquareClick={ () => handleClick(4)} />
+          <Square value = {squares[5]} onSquareClick={ () => handleClick(5)} />
+        </div>
+        <div className="board-row">
+          <Square value = {squares[6]} onSquareClick={ () => handleClick(6)} />
+          <Square value = {squares[7]} onSquareClick={ () => handleClick(7)} />
+          <Square value = {squares[8]} onSquareClick={ () => handleClick(8)} />
+        </div>
+        <a
+          href="http://localhost:3000/"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            戻る{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>   
+        </a>
       </div>
-      <div className="board-row">
-        <Square value = {squares[6]} onSquareClick={ () => handleClick(6)} />
-        <Square value = {squares[7]} onSquareClick={ () => handleClick(7)} />
-        <Square value = {squares[8]} onSquareClick={ () => handleClick(8)} />
-      </div>
-    </>
+    </main>
   );
 }
 
--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -108,8 +109,8 @@ export default function Home() {
           </p>
         </a>
 
-        <a
-          href="http://localhost:3000/TTT"
+        <Link
+          href="/TTT"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
@@ -123,7 +124,7 @@ export default function Home() {
           <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
             三目並べページに遷移します。
           </p>
-        </a>
+        </Link>
 
       </div>
     </main>
